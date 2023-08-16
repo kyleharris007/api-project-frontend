23c3fe7f4256400c3b9f9976edac0cc9092850f0
kyleharris007 <buccolt45@yahoo.com>
2023-08-16 15:13:09 -0400

    updated the app.js page with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import Nav from './components/Nav';
+import Home from './Screens/Home';
+import Quotes from './Screens/Quotes';
+import QuoteDetail from './Screens/QuoteDetail';
+import QuoteCreate from './Screens/QuoteCreate';
+import QuoteEdit from './Screens/QuoteEdit';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Seinfeld Quotes and Memes!</h1>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quotes" element={<Quotes />} />
+        <Route path="/quotes/:id" element={<QuoteDetail />} />
+        <Route path="/add-quote" element={<QuoteCreate />} />
+        <Route path="/quotes/:id/edit" element={<QuoteEdit />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
